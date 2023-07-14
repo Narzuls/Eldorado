4219827673e4d9e4135a180c146c4cae76c64a5f
Narzuls <andrew.ottavi@gmail.com>
2023-07-14 08:55:12 -0400

    added and removed some mod

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -2,7 +2,26 @@
 
 console.info('Hello, World! (You will see this line every time client resources reload)')
 
-onEvent('jei.hide.items', event => {
-	// Hide items in JEI here
-	// event.hide('minecraft:cobblestone')
+
+onEvent('rei.hide.items', e => {
+	e.hide([
+	  'ae2:facade',
+	  'jsonthings:sp_1',
+	  'jsonthings:sp_2',
+	  'jsonthings:sp_3',
+	  'jsonthings:sp_4',
+	  'jsonthings:sp_5',
+	  'jsonthings:sp_6',
+	  'jsonthings:sp_7',
+	  'jsonthings:sp_8',
+	  'excompressum:mana_hammer',
+	  'industrialforegoing:transporter',
+	  'titanium:block_test',
+	  'titanium:block_asset_test',
+	  'titanium:machine_test',
+	  'titanium:block_twenty_four_test',
+	  'titanium:creative_generator'
+	])
 })
+
+console.info('*****SCRIPT RELOADED*****')
