1bf3556ecc9e337d4b5886d32f1163cee474396e
Jazimalyboy <jazimalyboy@gmail.com>
2025-07-13 13:52:16 -0400

    Update 0.5
    Gregtech solar panel recipe add

--- kubejs/server_scripts/gtceu/gtceu_solar_panels.js
+++ kubejs/server_scripts/gtceu/gtceu_solar_panels.js
@@ -1,83 +1,133 @@
 ServerEvents.recipes(event => {
 
-  const panels = [
-    {
-      id: 'ulv_solar_panel',
-      plate: 'bronze_plate',
-      circuit: 'ulv_circuit',
-      battery: 'ulv_battery',
-      cable: 'copper_single_cable'
-    },
-    {
-      id: 'lv_solar_panel',
-      plate: 'steel_plate',
-      circuit: 'lv_circuit',
-      battery: 'lv_battery',
-      cable: 'tin_single_cable'
-    },
-    {
-      id: 'mv_solar_panel',
-      plate: 'aluminium_plate',
-      circuit: 'mv_circuit',
-      battery: 'mv_battery',
-      cable: 'copper_single_cable'
-    },
-    {
-      id: 'hv_solar_panel',
-      plate: 'stainless_steel_plate',
-      circuit: 'hv_circuit',
-      battery: 'hv_battery',
-      cable: 'gold_single_cable'
-    },
-    {
-      id: 'ev_solar_panel',
-      plate: 'titanium_plate',
-      circuit: 'ev_circuit',
-      battery: 'ev_battery',
-      cable: 'aluminium_single_cable'
-    },
-    {
-      id: 'iv_solar_panel',
-      plate: 'tungsten_steel_plate',
-      circuit: 'iv_circuit',
-      battery: 'iv_battery',
-      cable: 'platinum_single_cable'
-    },
-    {
-      id: 'luv_solar_panel',
-      plate: 'naquadah_alloy_plate',
-      circuit: 'luv_circuit',
-      battery: 'luv_battery',
-      cable: 'naquadah_single_cable'
-    },
-    {
-      id: 'zpm_solar_panel',
-      plate: 'duranium_plate',
-      circuit: 'zpm_circuit',
-      battery: 'zpm_battery',
-      cable: 'tungsten_copper_single_cable'
-    },
-    {
-      id: 'uv_solar_panel',
-      plate: 'trinium_plate',
-      circuit: 'uv_circuit',
-      battery: 'uv_battery',
-      cable: 'yttrium_barium_cuprate_single_cable'
-    }
-  ];
+  // Supprimer uniquement les recettes ULV et LV
+  event.remove({ output: 'gtceu:ulv_solar_panel' });
+  event.remove({ output: 'gtceu:lv_solar_panel' });
 
-  panels.forEach(p => {
-    event.shaped(`gtceu:${p.id}`, [
-      'PCP',
-      'CBC',
-      'GDG'
-    ], {
-      P: `gtceu:${p.plate}`,
-      C: `gtceu:${p.circuit}`,
-      B: `gtceu:${p.battery}`,
-      D: `gtceu:${p.cable}`,
-      G: 'minecraft:glass_pane'
-    });
+  // ULV
+  event.shaped('gtceu:ulv_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:bronze_plate',
+    C: '#gtceu:circuits/lv',
+    B: '#gtceu:batteries/ulv',
+    D: 'gtceu:lead_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:solar_panel'
+  });
+
+  // LV
+  event.shaped('gtceu:lv_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:steel_plate',
+    C: '#gtceu:circuits/lv',
+    B: '#gtceu:batteries/lv',
+    D: 'gtceu:tin_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:ulv_solar_panel'
+  });
+
+  // MV
+  event.shaped('gtceu:mv_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:aluminium_plate',
+    C: '#gtceu:circuits/mv',
+    B: '#gtceu:batteries/mv',
+    D: 'gtceu:copper_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:lv_solar_panel'
+  });
+
+  // HV
+  event.shaped('gtceu:hv_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:stainless_steel_plate',
+    C: '#gtceu:circuits/hv',
+    B: '#gtceu:batteries/hv',
+    D: 'gtceu:gold_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:mv_solar_panel'
+  });
+
+  // EV
+  event.shaped('gtceu:ev_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:titanium_plate',
+    C: '#gtceu:circuits/ev',
+    B: '#gtceu:batteries/ev',
+    D: 'gtceu:aluminium_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:hv_solar_panel'
+  });
+
+  // IV
+  event.shaped('gtceu:iv_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:tungsten_steel_plate',
+    C: '#gtceu:circuits/iv',
+    B: '#gtceu:batteries/iv',
+    D: 'gtceu:platinum_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:ev_solar_panel'
+  });
+
+  // LuV
+  event.shaped('gtceu:luv_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:naquadah_alloy_plate',
+    C: '#gtceu:circuits/luv',
+    B: '#gtceu:batteries/luv',
+    D: 'gtceu:osmium_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:iv_solar_panel'
+  });
+
+  // ZPM
+  event.shaped('gtceu:zpm_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:duranium_plate',
+    C: '#gtceu:circuits/zpm',
+    B: '#gtceu:batteries/zpm',
+    D: 'gtceu:naquadah_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:luv_solar_panel'
+  });
+
+  // UV
+  event.shaped('gtceu:uv_solar_panel', [
+    'PHP',
+    'CBC',
+    'GDG'
+  ], {
+    P: 'gtceu:trinium_plate',
+    C: '#gtceu:circuits/uv',
+    B: '#gtceu:batteries/uv',
+    D: 'gtceu:yttrium_barium_cuprate_single_cable',
+    G: '#forge:glass_panes/colorless',
+    H: 'gtceu:zpm_solar_panel'
   });
 
 });
