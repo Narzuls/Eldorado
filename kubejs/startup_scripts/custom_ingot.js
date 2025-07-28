2bcc518e2c7eb384dfac3b8c0413f4d0130b65fc
Jazimalyboy <jazimalyboy@gmail.com>
2025-07-28 13:40:38 -0400

    change and update many mods and clean up

--- kubejs/startup_scripts/custom_ingot.js
+++
@@ -1,6 +0,0 @@
-// Créer un lingot personnalisé
-StartupEvents.registry('item', event => {
-  event.create('custom_ingot')
-    .displayName('Custom Ingot')
-    .texture('kubejs:item/custom_ingot')
-})
